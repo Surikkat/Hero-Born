f0da9d4df4b9cf8061e505af906e38e02fd227c8
Egor Suraveykin <74894572+Surikkat@users.noreply.github.com>
2022-06-29 16:31:38 +0300

    Add files via upload

--- Assets/Scripts/GameBehavior.cs
+++ Assets/Scripts/GameBehavior.cs
@@ -11,6 +11,8 @@ public class GameBehavior : MonoBehaviour
     public string labelText = "Collect all 4 items and win your freedom!";
     public int maxItems = 1;
 
+    public bool showLossScreen = false;
+
     private int _itemCollected = 0;
     public int Items    
     {
@@ -37,8 +39,25 @@ public class GameBehavior : MonoBehaviour
         set
         {
             _playerHP = value;
+
+            if(_playerHP <= 0)
+            {
+                labelText = "You want another life with that?";
+                showLossScreen = true;
+                Time.timeScale = 0;
+            }
+            else
+            {
+                labelText = "Ouch... that's got hurt.";
+            }
     
         }
+    }
+
+    void RestartLevel()
+    {
+        SceneManager.LoadScene(0);
+        Time.timeScale = 1.0f;
     }
 
     void OnGUI()
@@ -51,8 +70,15 @@ public class GameBehavior : MonoBehaviour
         {
             if(GUI.Button(new Rect(Screen.width/2-100,Screen.height/2-50,200,100), "YOU WON!"))
             {
-                SceneManager.LoadScene(0);
-                Time.timeScale = 1.0f;  
+                RestartLevel();  
+            }
+        }
+
+        if(showLossScreen)
+        {
+            if(GUI.Button(new Rect(Screen.width/2-100,Screen.height/2-50,200,100),"You lose..."))
+            {
+                RestartLevel();
             }
         }
     }
