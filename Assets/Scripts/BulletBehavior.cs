595511c3e0d39cbdad5b320adb9c517cd28dbce0
Egor Suraveykin <74894572+Surikkat@users.noreply.github.com>
2022-06-30 10:35:11 +0300

    Update BulletBehavior.cs

--- Assets/Scripts/BulletBehavior.cs
+++ Assets/Scripts/BulletBehavior.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Класс описывающий поведение снарядов
 public class BulletBehavior : MonoBehaviour
 {
     public float onscreenDelay = 3f;
 
     void Start()
     {
+        //После появления снаряд исчезает чтобы не загромождать сцену объектами
         Destroy(this.gameObject, onscreenDelay);
     }
 }
