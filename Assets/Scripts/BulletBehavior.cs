505673b114ceebfc69e598b3ecfc9123cc8a357a
Egor Suraveykin <74894572+Surikkat@users.noreply.github.com>
2022-07-03 09:56:42 +0300

    Update BulletBehavior.cs

--- Assets/Scripts/BulletBehavior.cs
+++ Assets/Scripts/BulletBehavior.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//Поведение пули
 public class BulletBehavior : MonoBehaviour
 {
     public float onscreenDelay = 3f;
 
     void Start()
     {
+        //Пуля будет исчезать спустя какое то время после появления чтобы на сцене не было много объектов
         Destroy(this.gameObject, onscreenDelay);
     }
 }
